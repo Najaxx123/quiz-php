0034ccab4c97a7d24c397f8ae5527e554f8664c8
Najaxx123 <romain.brival@outlook.fr>
2026-05-12 14:02:41 +0200

    Fix migration

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+#[Table('users')]
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
